6f1701ef5890e463867cbffdb82317eac2bef258
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-05-13 21:14:18 +0000

    done

---
+++ basics_01/3_conversionandoperations.js
@@ -0,0 +1,12 @@
+let score = "Soumyadeep"
+
+/*console.log(typeof score)
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)*/
+
+
+let isLoggedIn= ""
+let numberIsLoggedin = Boolean (isLoggedIn)
+console.log(numberIsLoggedin)
