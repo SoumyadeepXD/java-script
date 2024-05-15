1954bc15465dcaada7dd14a30d955cb27549a639
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-05-15 21:27:57 +0000

    done

---
+++ basics_01/6_numbers_and_maths.js
@@ -0,0 +1,20 @@
+let age = 18
+console.log(age)
+
+let newage = new Number(29)
+console.log(newage); 
+
+const number = 69.6969
+console.log(number.toPrecision(2))
+const Hundreds = 10000000
+console.log(Hundreds.toLocaleString)
+console.log(Hundreds.toLocaleString('en-IN'))
+//+++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(Math.abs(-69));
+console.log(Math.round(69.69));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(4,6,7,8,5,6,7,3,2,12));
+console.log(Math.max(4,6,7,8,5,6,7,3,2,12));
+console.log((Math.floor(Math.random()*100)));
+
