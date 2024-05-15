dc7ad265182761d7ed386095c742cf44cae1d347
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-05-15 20:58:29 +0000

    done

---
+++ basics_01/5_string.js
@@ -0,0 +1,15 @@
+const Name="Soumyadeep"
+const age =18
+
+console.log(`Hi, I am ${Name} a ${age} years old male.`)
+
+const gamename = new String('Soumyadeep')
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf('y'));
+const newString = gamename.substring(0,6)
+console.log(newString)
+const anotherString= gamename.slice(10,5)
+console.log(anotherString);
