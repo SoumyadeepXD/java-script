36e2e1ae704cd3109c6d1d3649b6dbbaf0c4586f
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-05-17 18:01:32 +0000

    done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,12 @@
+//array
+const myArray = new Array(1,2,3,4,5)
+console.log(myArray[2])
+
+myArray.push(69)
+console.log(myArray)
+myArray.pop()
+console.log(myArray)
+myArray.unshift(9)
+console.log(myArray)
+myArray.shift()
+console.log(myArray)
