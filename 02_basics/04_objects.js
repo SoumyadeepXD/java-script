19254be70d4a468ca12b186df7e3aa32673cb1aa
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-05-18 20:35:00 +0000

    done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,13 @@
+// const tinderuser = new Object()
+const tinderuser = {}
+
+tinderuser.Id ="69169"
+tinderuser.name = "Soumyadeep"
+tinderuser.email = "soumyadeep.google.com"
+tinderuser.isLoggedin = false
+
+//console.log(tinderuser)
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj3 = {obj1, obj2}
+console.log(obj3)
