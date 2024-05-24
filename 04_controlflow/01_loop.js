65207a0744b209b9bfc93cde80c898c8b3b6ea7c
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-05-24 18:13:33 +0000

    done

---
+++ 04_controlflow/01_loop.js
@@ -0,0 +1,28 @@
+//we can use various operators to check if it is true or not
+/*
+< checks if its less or not
+>checks if its greater than or not
+<= checks either it less or equal
+>= checks either it is greater or equal
+== checks if both values are equal or not
+===(strict equal) it checks if both the values are same as well as the data types of those values.
+
+
+*/
+// let temp = 32
+// if(temp ==42){
+//     console.log("temperature is plesent 41 degree celsius")
+    
+// } else{
+//     console.log(`temperature is greater than ${temp}`)
+// }
+// const score = 32
+// if(score < 40){
+//     const power = "fly"
+//     console.log(`user power: ${power}`)
+// }
+
+// console.log(`user power: ${power}`)
+
+// *********************SHORT HAND NOTATION***************************
+
