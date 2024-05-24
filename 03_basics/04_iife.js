dd9a7f42f4b8501264f74336807e67fabbfd6169
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-05-24 17:01:44 +0000

    done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+//IIFE
+
+(function gg(){
+    //named  iife
+    console.log(`DB connected`)
+
+})();
+
+
+//(defination of Function)(execution)
+((name)=>{
+    console.log(`why you gay ${name}`)
+})('anish')
