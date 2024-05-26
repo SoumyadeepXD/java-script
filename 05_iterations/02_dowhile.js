9b6ccace1ed0428dc926fd8e9a3bf4cdaae4a161
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-05-26 20:34:07 +0000

    done

---
+++ 05_iterations/02_dowhile.js
@@ -0,0 +1,22 @@
+//  WHILE
+
+let i = 0
+while (i<=10) {
+    console.log(`value of i is : ${i}`)
+    i = i+1
+    
+}
+let myArr = ["spiderman", "ironman", "black panther"]
+let arr = 0
+while (arr<myArr.length) {
+    console.log(`My Super hero ranking ${myArr[arr]}, #${arr+1}`)
+    arr = arr+1
+}
+
+
+//DO WHILE
+let j = 0
+do {
+    console.log(`i am noob`)
+    j++
+} while (j<=5);
