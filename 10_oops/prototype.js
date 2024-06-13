078c58d9768f6cb79c0a6a7c52bfe6f34dde16b9
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-06-13 19:09:07 +0000

    dd

--- 10_oops/prototype.js
+++ 10_oops/prototype.js
@@ -1,2 +1,19 @@
-let Myname = "soumyadeep     "
-console.log(Myname.trim().length)
+// let Myname = "soumyadeep     "
+// console.log(Myname.trim().length)
+
+
+let myheros=['spiderman',"hawkeye"]
+
+let heropower={
+    spiderman: "spidersence",
+    hawkeye: "bow and arrow",
+    getpower:function(){
+        console.log(`spiderman has ${this.spiderman}`)
+    }
+}
+
+Object.prototype.soumya=function () {
+    console.log("amar mathay kichu dhukche na")    
+}
+heropower.soumya()
+myheros.soumya()
