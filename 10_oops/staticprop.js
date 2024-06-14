107d6c4b603e0975162dcf28aaf19c1efd7db0e4
Soumyadeep Dolui <85886212+SoumyadeepXD@users.noreply.github.com>
2024-06-14 20:38:25 +0000

    d

---
+++ 10_oops/staticprop.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+
+const me = new User('soumyadeep')
+
+me.logMe()
